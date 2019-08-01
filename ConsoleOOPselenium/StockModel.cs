b0529a83a0fa4c88b646be21e4a7bc0d26c1cb05
Greg <gregsmith446@gmail.com>
2019-08-01 12:57:59 -0400

    works but for 1 stock then 'invalid session id'

---
+++ ConsoleOOPselenium/StockModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleOOPselenium
+{
+    public class StockModel
+    {
+        public string Symbol { get; set; }
+        public string Price { get; set; }
+        public string PChange { get; set; }
+        public string Volume { get; set; }
+        public string MarketCap { get; set; }
+    }
+}
